d7431e57119088e1dea6d2700e026184271772c0
Yan Shuaibin <Extern.Shuaibin.Yan@mobilityasia.com.cn>
2023-11-27 10:33:32 +0800

    update DeviceInfo's json format

--- device_info.go
+++ device_info.go
@@ -9,15 +9,15 @@ import (
 
 type DeviceInfo struct {
 	// Always set.
-	Serial string
+	Serial string `json:"serial"`
 
 	// Product, device, and model are not set in the short form.
-	Product    string
-	Model      string
-	DeviceInfo string
+	Product    string `json:"product"`
+	Model      string `json:"model"`
+	DeviceInfo string `json:"deviceInfo"`
 
 	// Only set for devices connected via USB.
-	Usb string
+	Usb string `json:"usb"`
 }
 
 // IsUsb returns true if the device is connected via USB.
